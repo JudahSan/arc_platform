7c66f6ebac00da57b0474dedf2ee43eac2848d60
JudahSan <baraka.judah@yahoo.com>
2023-11-01 00:31:44 +0300

    Fixed RuboCop Linter errors

--- test/controllers/learning_resources_controller_test.rb
+++ test/controllers/learning_resources_controller_test.rb
@@ -1,7 +1,30 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class LearningResourcesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+require 'test_helper'
+
+class LRControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @learning_resource = learning_resources(:one) # Assuming you have fixtures set up
+    @user = users(:one) # Assuming you have users fixtures set up
+    sign_in @user
+  end
+
+  test 'should get index' do
+    get learning_resources_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_learning_resource_url
+    assert_response :success
+  end
+
+  test 'should create learning resource' do
+    assert_difference('LearningResource.count') do
+      post learning_resources_url,
+           params: { learning_resource: { title: 'Sample Title', description: 'Sample Description', link: 'https://example.com', expertise_level: 'Beginner' } }
+    end
+
+    assert_redirected_to learning_resources_url
+  end
 end
