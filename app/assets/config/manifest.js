f08918b80bbf4fe19bac20a4c0a2f10722d422ee
judahsan <baraka.judah@gmail.com>
2025-08-19 03:32:52 +0300

    chore: update asset pipeline configuration and CI workflow
    - Add CSS builds directory to asset paths
    - Update manifest to exclude JS compilation via Sprockets
    - Modify CI workflow tasks to align with asset pipeline changes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,7 @@
 //= link_tree ../builds
 //= link mailgun_mails.css
 //= link application.tailwind.css
-//= link_tree ../../javascript .js
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+
+// Note: With importmap-rails, JavaScript is loaded via `javascript_importmap_tags`
+// and not through Sprockets. Do not link app/javascript or vendor/javascript here,
+// otherwise Sprockets will try to precompile JS and may expect a bundled application.js.
