d03a0088b9ebfca88a7a7ed2a8ea213c7d9d0d5b
judahsan <baraka.judah@gmail.com>
2025-08-20 21:36:59 +0300

    Revert "chore: add docs and update CI workflow dependencies and environment setup"
    This reverts commit cc76d07c43f44150b34d06ddeb9ac3cf49eda4d0.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,6 @@
 
 ##
 # Devise override Registration controller
-# Adds Cloudflare Turnstile verification before creating an account.
-# Expects the front-end to provide the token param name 'cf-turnstile-response'.
-# See: app/views/shared/_cloudflare_turnstile.html.erb and
-#      app/javascript/controllers/turnstile_controller.js
 module Users
   class RegistrationsController < Devise::RegistrationsController
     ##
@@ -21,8 +17,6 @@ module Users
 
     private
 
-    # Returns early if server-side verification succeeds; otherwise prepares the
-    # form with an error and re-renders the page.
     def verify_turnstile
       token = params['cf-turnstile-response']
       return if TurnstileVerifier.new(token, request.remote_ip).verify
@@ -30,8 +24,6 @@ module Users
       handle_failed_turnstile_verification
     end
 
-    # Renders the sign up form with an error message.
-    # The message is sourced from I18n at 'turnstile.errors.registration_failed'.
     def handle_failed_turnstile_verification
       self.resource = resource_class.new(sign_in_params)
       clean_up_passwords(resource)
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,11 +2,6 @@
 
 module Users
   class SessionsController < Devise::SessionsController
-    # Turnstile protection
-    # Verifies the Cloudflare Turnstile token before allowing user sign in.
-    # Expects the front-end to provide the token param name 'cf-turnstile-response'.
-    # See: app/views/shared/_cloudflare_turnstile.html.erb and
-    #      app/javascript/controllers/turnstile_controller.js
     # invisible_captcha only: [:create], honeypot: :nickname
     before_action :verify_turnstile, only: :create
 
@@ -18,8 +13,6 @@ module Users
 
     private
 
-    # Returns early if server-side verification succeeds; otherwise prepares the
-    # form with an error and re-renders the page.
     def verify_turnstile
       token = params['cf-turnstile-response']
       return if TurnstileVerifier.new(token, request.remote_ip).verify
@@ -27,8 +20,6 @@ module Users
       handle_failed_turnstile_verification
     end
 
-    # Renders the sign in form with an error message.
-    # The message is sourced from I18n at 'turnstile.errors.login_failed'.
     def handle_failed_turnstile_verification
       self.resource = resource_class.new(sign_in_params)
       clean_up_passwords(resource)
