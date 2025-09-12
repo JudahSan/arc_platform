429f9e77c9d6d71de83a8f67e58ffa2e76ca7fc0
judahsan <baraka.judah@gmail.com>
2025-09-13 00:31:13 +0300

    Revert "fix(learning_materials): Fix access issue with learning materials in motor admin"
    This reverts commit cb6400236996bb54e394c3960a670313c53a44a6.

--- test/controllers/learning_materials_controller_test.rb
+++ test/controllers/learning_materials_controller_test.rb
@@ -44,7 +44,7 @@ class LearningMaterialsControllerTest < ActionDispatch::IntegrationTest
   #     LearningMaterial.create!(
   #       title: "Material #{i}",
   #       level: :beginner,
-  #       link: "https://example.com/#{i}"
+  #       link_url: "https://example.com/#{i}"
   #     )
   #   end
   #
