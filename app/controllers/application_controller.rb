391b1fc91985c233cdade60ba34d24a3eefb5478
judahsan <baraka.judah@gmail.com>
2025-09-12 23:19:46 +0300

    testing fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
 
   before_action :authenticate_user! # All users should be authenticated in all controllers by default
   before_action :configure_permitted_parameters, if: :devise_controller?
