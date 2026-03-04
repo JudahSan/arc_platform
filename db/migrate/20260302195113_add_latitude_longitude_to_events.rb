5cdc72951b9558a07e07a16c41210b572383fffe
judahsan <baraka.judah@gmail.com>
2026-03-04 20:31:28 +0300

    fix deployment

--- db/migrate/20260302195113_add_latitude_longitude_to_events.rb
+++ db/migrate/20260302195113_add_latitude_longitude_to_events.rb
@@ -1,6 +1,6 @@
 class AddLatitudeLongitudeToEvents < ActiveRecord::Migration[8.1]
   def change
-    add_column :events, :latitude, :decimal, precision: 10, scale: 8, if_not_exists: true
-    add_column :events, :longitude, :decimal, precision: 11, scale: 8, if_not_exists: true
+    add_column :events, :latitude, :decimal, precision: 10, scale: 8 unless column_exists?(:events, :latitude)
+    add_column :events, :longitude, :decimal, precision: 11, scale: 8 unless column_exists?(:events, :longitude)
   end
 end
