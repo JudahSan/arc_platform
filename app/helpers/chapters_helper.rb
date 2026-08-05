4d0d884309473e727187542b5d7b01882d967c26
judahsan <baraka.judah@gmail.com>
2026-08-06 02:18:48 +0300

    feat: migrate to TailwindCSS and add reusable SVG icon helper
    - Replaced `cssbundling-rails` with `tailwindcss-rails` for streamlined styling and configuration.
    - Created `IconHelper` to easily render inline SVG icons with customizable classes.
    - Added reusable SVG icons for GitHub, Telegram, LinkedIn, and Instagram.
    - Upgraded Node.js to version 22.23.2 for compatibility.
    - Refactored global styles and components to align with TailwindCSS.
    - Improved maintainability by migrating styles to a standardized TailwindCSS structure.

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -45,17 +45,42 @@ module ChaptersHelper
   ].freeze
 
   SOCIALS = [
-    { alt_key: 'social_media.twitter', link: 'https://twitter.com/ruby_african', image: 'brands_twitter.png',
-      show: true },
-    { alt_key: 'social_media.telegram', link: '#', image: 'brands_telegram.png',
-      show: FeatureFlag.find_by(name: 'telegram')&.enabled },
-    { alt_key: 'social_media.facebook', link: 'https://www.facebook.com/rubycommunity.africa',
-      image: 'brands_facebook.png',
-      show: true },
-    { alt_key: 'social_media.instagram', link: 'https://www.instagram.com/africanruby_community/', image: 'brands_instagram.png', show: true },
-    { alt_key: 'social_media.linkedin', link: 'https://www.linkedin.com/company/african-ruby-community/',
-      image: 'brands_linkedin.png', show: true },
-    { alt_key: 'social_media.github', link: 'https://github.com/nairuby', image: 'brands_github.png', show: true }
+    {
+      alt_key: 'social_media.x',
+      link: 'https://x.com/ruby_african',
+      icon: 'icons/x.svg',
+      show: true
+    },
+    {
+      alt_key: 'social_media.telegram',
+      link: '#',
+      icon: 'icons/telegram.svg',
+      show: FeatureFlag.find_by(name: 'telegram')&.enabled
+    },
+    {
+      alt_key: 'social_media.facebook',
+      link: 'https://www.facebook.com/rubycommunity.africa',
+      icon: 'icons/facebook.svg',
+      show: true
+    },
+    {
+      alt_key: 'social_media.instagram',
+      link: 'https://www.instagram.com/africanruby_community/',
+      icon: 'icons/instagram.svg',
+      show: true
+    },
+    {
+      alt_key: 'social_media.linkedin',
+      link: 'https://www.linkedin.com/company/african-ruby-community/',
+      icon: 'icons/linkedin.svg',
+      show: true
+    },
+    {
+      alt_key: 'social_media.github',
+      link: 'https://github.com/nairuby',
+      icon: 'icons/github.svg',
+      show: true
+    }
   ].freeze
 
   def activities
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Community number
+  config.community_whatsapp_number = "2547799690818"
 end
